5faa41d1f5c168b95210936859efd25f6e16b9ee
watal <watal.i27e@gmail.com>
2018-11-10 13:59:26 +0900

    Comply with PEP8

--- xmltojson.py
+++ xmltojson.py
@@ -4,6 +4,7 @@ import sys
 import json
 import xmltodict
 
+
 def xmltojson(path):
     # xmlファイルの読み込み
     with open(path, 'r') as xml_file:
@@ -16,16 +17,18 @@ def xmltojson(path):
     with open('data/Dam.json', 'w') as rslt_file:
         json.dump(tmp_dict, rslt_file, ensure_ascii=False, indent=2)
 
+
 def main():
     # 引数指定
     args = sys.argv
     argc = len(args)
 
     if (argc != 2):
-        print ('Usage:  python %s [FILE]' % args[0])
+        print('Usage:  python %s [FILE]' % args[0])
         quit()
 
     xmltojson(args[1])
+
 
 if __name__ == '__main__':
     main()
